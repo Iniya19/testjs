bc0e67dffa9505975839f8499ba648bf042c6a59
Inna Zhuravlova <inessa38@gmail.com>
2022-01-23 19:43:28 +0100

    task2

--- js/tasks.js
+++ js/tasks.js
@@ -295,3 +295,26 @@
 // }
 
 // findTheColor('red')
+//Напишите ф-цию calcTotalPrice(stones, stonesName),
+//которая принимает массив объектов и
+//строку с названием камня.
+//Функция считает м возвращает общую стоимость камней
+//с таким именем, ценой и количеством из объекта
+
+// const stones = [
+//   { name: "Изумруд", price: 1300, quantity: 4 },
+//   { name: "Бриллиант", price: 2700, quantity: 6 },
+//   { name: "Сапфир", price: 400, quantity: 7 },
+//   { name: "Щебень", price: 150, quantity: 100 },
+// ];
+
+// function calcTotalPrice(stones, stonesName) {
+//     for (const {name, price, quantity} of stones) {
+//         if (name === stonesName) { 
+//             return price * quantity;
+//         }
+//     }  
+//     }
+// console.log(calcTotalPrice(stones, 'Щебень'));
+
+
